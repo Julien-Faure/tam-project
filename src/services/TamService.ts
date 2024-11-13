52ab36b01ea5844714df68294467ab5e783a875d
Julien Faure <julien.faure02@etu.umontpellier.fr>
2024-11-13 21:18:16 +0100

    WIP : getDelayByLineAndStop implementation in service layer

--- src/services/TamService.ts
+++ src/services/TamService.ts
@@ -1,6 +1,10 @@
 import {LineModel} from "@/models/LineModel";
+import {DelayModel} from "@/models/DelayModel";
+import {StopModel} from "@/models/StopModel";
 
 
 export interface TamService {
     getLines() : Promise<LineModel[]>;
+    getDelayByLineAndStop(lineId : string, stopId : string) : Promise<DelayModel>;
+    getStop(stopId : string) : Promise<StopModel>;
 }
--- src/services/TamServiceImpl.ts
+++ src/services/TamServiceImpl.ts
@@ -1,6 +1,10 @@
 import {TamService} from "@/services/TamService";
 import {LineInfosDto} from "@/services/dto/LineInfosDto";
 import {LineModel} from "@/models/LineModel";
+import {types} from "sass";
+import Error = types.Error;
+import {DelayModel} from "@/models/DelayModel";
+import {StopModel} from "@/models/StopModel";
 
 
 export class TamServiceImpl implements TamService {
@@ -32,4 +36,25 @@ export class TamServiceImpl implements TamService {
                 number: line.numero
             }
     }
+
+    async getDelayByLineAndStop(lineId: string, stopId: string): Promise<DelayModel> {
+        const delayResponse = await fetch(`${this.apiUrl}/gtfs/ligne/${lineId}/ordered-arrets`);
+        const stopResponse = await fetch(`${this.apiUrl}/gtfs/ligne/${lineId}/physicalstops`);
+
+
+        //TODO : Do magic tricks to get delay by line and stop
+
+        if (!delayResponse.ok) throw new Error("Unable to fetch lines from API.");
+
+        const lineStops = await delayResponse.json() as any;
+
+
+        return Promise.resolve(undefined);
+    }
+
+    getStop(stopId: string): Promise<StopModel> {
+        return Promise.resolve(undefined);
+    }
+
+
 }

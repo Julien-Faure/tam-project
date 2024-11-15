8bd8c2fe1f1421febe0fd92669c3372169369959
Julien Faure <julien.faure02@etu.umontpellier.fr>
2024-11-15 17:33:05 +0100

    Update HotLineView to dynamically set UI colors based on line ID.

--- src/views/HotLineView.vue
+++ src/views/HotLineView.vue
@@ -1,17 +1,16 @@
-
 <template>
-    <v-timeline side="end" dot-color="primary">
+    <v-timeline side="end" :dot-color="ui.mainColor">
         <v-timeline-item
             v-for="item in ds"
             :key="item.stop.id"
             size="small"
         >
-            <v-card width="500px">
+            <v-card width="400px">
                 <v-card-title>
-                    {{item.stop.name}}
+                    {{ item.stop.name }}
                 </v-card-title>
                 <v-card-text>
-                    <v-chip color="primary" v-for="d in item.delay">{{ d.delayCount }} sec</v-chip>
+                    <v-chip :color="ui.mainColor" v-for="d in item.delay">{{ d.delayCount }} sec</v-chip>
                 </v-card-text>
             </v-card>
         </v-timeline-item>
@@ -23,13 +22,17 @@ import {Vue, Component} from 'vue-facing-decorator';
 import {Context} from "@/Context";
 import {useTamStore} from "@/stores";
 import {DelayStopModel} from "@/models/DelayStopModel";
+import {linesColors} from "@/utils/TamColorMap";
 
 @Component({})
 export default class HotLineView extends Vue {
     private readonly tamService = Context.provideTamService();
 
+    private readonly ui = {
+        mainColor: 'primary',
+    }
 
-    private ds : DelayStopModel[] = [];
+    private ds: DelayStopModel[] = [];
 
     private async mounted() {
         const lineId = this.$route.params.lineId as string;
@@ -38,6 +41,8 @@ export default class HotLineView extends Vue {
         await tamStore.refreshAllDSByLine(lineId);
 
         this.ds = tamStore.getAllDSByLine(lineId);
+
+        this.ui.mainColor = linesColors.get(lineId) ?? 'primary';
     }
 }
 </script>

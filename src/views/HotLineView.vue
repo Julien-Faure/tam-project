f6d3635498efe6b9081cc94e4ab01b824e29d6cf
Julien Faure <julien.faure02@etu.umontpellier.fr>
2024-11-15 17:20:56 +0100

    Implement timeline display for DelayStopModel data in HotLineView component.

--- src/views/HotLineView.vue
+++ src/views/HotLineView.vue
@@ -1,14 +1,28 @@
+
+<template>
+    <v-timeline side="end">
+        <v-timeline-item
+            v-for="item in ds"
+            :key="item.stop.id"
+            size="small"
+        >
+            {{item.stop.name}}
+        </v-timeline-item>
+    </v-timeline>
+</template>
+
 <script lang="ts">
 import {Vue, Component} from 'vue-facing-decorator';
 import {Context} from "@/Context";
 import {useTamStore} from "@/stores";
+import {DelayStopModel} from "@/models/DelayStopModel";
 
 @Component({})
 export default class HotLineView extends Vue {
     private readonly tamService = Context.provideTamService();
 
 
-    private data? : string;
+    private ds : DelayStopModel[] = [];
 
     private async mounted() {
         const lineId = this.$route.params.lineId as string;
@@ -16,14 +30,11 @@ export default class HotLineView extends Vue {
         const tamStore = useTamStore();
         await tamStore.refreshAllDSByLine(lineId);
 
-        this.data = JSON.stringify(tamStore.getAllDSByLine(lineId));
+        this.ds = tamStore.getAllDSByLine(lineId);
     }
 }
 </script>
 
-<template>
-    {{this.data}}
-</template>
 
 <style scoped>
 
